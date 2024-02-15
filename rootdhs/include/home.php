8e42f8540f7101035b5eb08e0088d689cae41f25
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-02-14 22:21:10 -0500

    Adding lab page content and more content include style cleanup simplifying styles

--- rootdhs/include/home.php
+++ rootdhs/include/home.php
@@ -1,8 +1,10 @@
 <div id="main">
-    <h1 class="centeredtext tealheader">Site under construction</h1>
-    <p class="centeredtext">
-		Welcome to Snailbird!  The Lab is home to homebrew software for computer musicians.  
-        The Studio is home to music made with electronic machines.  
-        Feel free to explore as the site expands to host my projects.
-    </p>
+    <div class="post">
+        <h1 class="centeredtext tealheader">Site under construction</h1>
+        <p class="centeredtext">
+            Welcome to Snailbird!  The Lab is home to homebrew software for computer musicians.  
+            The Studio is home to music made with electronic machines.  
+            Feel free to explore as the site expands to host my projects.
+        </p>
+    </div>
 </div>
---
+++ rootdhs/include/lab.php
@@ -0,0 +1,24 @@
+<div id="main">
+    <div class="post">
+        <h2 class="tealheader">Snailbird Lab</h2>
+        <p>
+            Recently, after lots of reading, I still didn't know where to start in building my own 
+            musical sofware tools.
+            The idea for Patch Notes came after some time thinking of solutions for managing the
+            programs and patching for an array of hardware synthesizers and MIDI instruments.  
+            Inspired by Nick Batt's use of Yamaha Setlist for iOS, I thought of creating a similar 
+            solution to embedding patching notes into a DAW track for reference during a live session.
+        </p>
+
+        <h3 class="tealheader">Patch Notes</h3>
+        <p>
+            Patch Notes is a Digital Audio Workstation plugin designed to afford hardware synthesizer 
+            players an easier experience managing patch settings between DAW sessions.  
+            More than a system for remembering and issuing Program Change messages, Patch Notes aims
+            to cater to analog or other non-MIDI enabled gear, allowing the musician quick access to 
+            critical information related to setting up a patch.  Patch Notes is currently in 
+            design and development, and you can track the progress on 
+            <a href="https://github.com/daniel-c-harvey/PatchNotes">github</a>.
+        </p>
+    </div>
+</div>
