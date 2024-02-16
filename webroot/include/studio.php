f2af7b69213dee7a1eed80a2739d49cc381fb1e1
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-02-16 07:06:02 -0500

    starting studio page, bandcamp player flowout still need to center the twocolumn cell content

--- webroot/include/studio.php
+++ webroot/include/studio.php
@@ -3,14 +3,25 @@
     <div class="post">
         <h2 class="pinkheader">2020</h2>
         <div class="row threecolumn">
-            <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=1972876996/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
-            <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=1448885517/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
-            <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=3640160562/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
+            <div class="cell">
+                <!-- <img src="https://cdn.worldvectorlogo.com/logos/michelin-2.svg" alt="michelin man"> -->
+                <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=1972876996/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
+            </div>
+            <div class="cell">
+                <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=1448885517/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
+            </div>
+            <div class=" cell">
+                <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=3640160562/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
+            </div>
         </div>
         <h2 class="pinkheader">2019</h2>
         <div class="row twocolumn">
-            <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1168376895/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=2978279851/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/i-hate-you-more-th-n-you-hate-me">i hate you more thæn you Hate me by Square Peg</a></iframe>
-            <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1168376895/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=4120804156/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/i-hate-you-more-th-n-you-hate-me">i hate you more thæn you Hate me by Square Peg</a></iframe>
+            <div class="cell">
+                <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1168376895/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=4120804156/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/i-hate-you-more-th-n-you-hate-me">i hate you more thæn you Hate me by Square Peg</a></iframe>
+            </div>
+            <div class="cell">
+                <iframe class="bandcampplayer" src="https://bandcamp.com/EmbeddedPlayer/album=1168376895/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=2978279851/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/i-hate-you-more-th-n-you-hate-me">i hate you more thæn you Hate me by Square Peg</a></iframe>
+            </div>
         </div>
     </div>
 </div>
