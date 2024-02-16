b858bee93e1eb53c3404807d03215ec3c976e824
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-02-15 21:37:17 -0500

    repo maintenance - rename and associated adjustments

--- webroot/include/studio.php
+++ webroot/include/studio.php
@@ -1,6 +1,6 @@
 <div id="main">
     <div class="post">
-        <h2>test</h2>
+        <h2 class="pinkheader">2020</h2>
         <p>
             <iframe style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/album=1156667419/size=large/bgcol=333333/linkcol=9a64ff/minimal=true/track=1972876996/transparent=true/" seamless><a href="https://sprh.bandcamp.com/album/theres-nobody-here-now">There&#39;s Nobody Here Now by Square Peg</a></iframe>
         </p>
