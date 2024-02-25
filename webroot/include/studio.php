452ca4c9e957fb9d3e56981ab8b57cc7d93a7695
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-02-25 14:03:29 -0500

    fixing and expanding code for post collapse and expansion.  more unlinked content drafts

--- webroot/include/studio.php
+++ webroot/include/studio.php
@@ -1,17 +1,19 @@
 <div id="main">
     <?php
         // Define valid routes here.  Maybe move to config at some point...
-        $valid_pages = ['studio/squarepeg'];
+        $valid_pages = ['square-peg-releases'];
 
         // Check for subpage routings first
         if (isset($_GET['page'])) {
             $page = htmlspecialchars_decode($_GET['page']);
             if (in_array($page, $valid_pages, false)) {
-                include("include/$page.php");
+                include("include/studio/$page.php");
             }
         } else {
             // Show post feed by default
             $posts = [[1, 'square-peg-history'], [2, 'test']];
+
+            // design a post feed mechanism to inject throughout the site, Home, other defaults
 
             foreach($posts as $post)
             {
