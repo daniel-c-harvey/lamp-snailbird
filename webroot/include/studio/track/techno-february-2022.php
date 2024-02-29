4d6c261225827d00a44adf1cbe9d78bd53017185
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-02-28 21:10:49 -0500

    styles for song detail pages

--- webroot/include/studio/track/techno-february-2022.php
+++ webroot/include/studio/track/techno-february-2022.php
@@ -1,10 +1,24 @@
 <div class="post">
     <h2 class="aqua header">February 2022</h3>
-    <audio controls src="audio/techno-february-2022.mp3" />
-    <p>Title: Untitled</p>
-    <p>Drums: Phase Plant, Serum</p>
-    <p>Bass: Phase Plant</p>
-    <p>Growl Lead / Stabs: Bass Station 2 into Boss GE-7 into Darkglass B3K V2 into Zoom MS60B running the SVT and Tape Echo</p>
-    <p>Glassy Arp Lead: Minilogue XD</p>
-    <p>Jingling Phaser Atmospheres: Minilogue XD</p>
+    <audio controls src="audio/techno-february-2022.mp3"></audio>
+    <ul class="songdetails">
+        <li>
+            <span class="instrument">Title:</span> Untitled
+        </li>
+        <li>
+        <span class="instrument">Drums:</span> Phase Plant, Serum
+        </li>
+        <li>
+        <span class="instrument">Bass:</span> Phase Plant
+        </li>
+        <li>
+        <span class="instrument">Growl Lead / Stabs:</span> Bass Station 2 into Boss GE-7 into Darkglass B3K V2 into Zoom MS60B running the SVT and Tape Echo
+        </li>
+        <li>
+        <span class="instrument">Glassy Arp Lead:</span> Minilogue XD
+        </li>
+        <li>
+        <span class="instrument">Jingling Phaser Atmospheres:</span> Minilogue XD
+        </li>
+    </ul>
 </div>
