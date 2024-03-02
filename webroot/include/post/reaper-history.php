d32e4a0ab9bc7fc262c6fbdf825ea64d767b191e
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-03-02 08:28:30 -0500

    more blogging

--- webroot/include/post/reaper-history.php
+++ webroot/include/post/reaper-history.php
@@ -1,25 +1,31 @@
-<h1 class="pink header">My REAPER Origins</h1>
-<h3 class="aqua date header">2024 02 21</h3>
+<h1 class="pink header">From Software, Hardware, to Everything at Once</h1>
+<h3 class="aqua date header">2024 03 01</h3>
 <p>    
-    <a href="https://reaper.fm">REAPER</a> has become the beating heart of how I make and play music, 
-    and to talk about some of my current use cases and workflows for playing live with synthesizers, 
-    I thought it may make more sense to start at the beginning.
+    <a href="https://reaper.fm">REAPER</a> has become the beating heart of how 
+    I make and play music, and to talk about some of my current use cases and 
+    workflows for playing live with synthesizers, I thought it may make more 
+    sense to start at the beginning.
 </p>
 <img class="centeredimage" src="images/REAPER/SIV-tracking-session.jpg" />
 <p>
-    I started using the software for basic multitrack recording of a metal band back in 2012 and then as I moved and lost access to a space to play drums, 
+    I started using the software for basic multitrack recording of a metal band back in 
+    2012 and then as I moved and lost access to a space to play drums, 
     I focused all my creativity into in-the-box synthwave-ish productions in REAPER. 
-    Having never made or really even listened to very much electronic music, my first influences were an eclectic blend of artists such as 
+    Having never made or really even listened to very much electronic music, 
+    my first influences were an eclectic blend of artists such as 
     <a href="https://autechre.bandcamp.com/track/further">Autechre</a>, 
-    <a href="https://blankbanshee.bandcamp.com/track/b-start-up">Blank</a> <a href="https://blankbanshee.bandcamp.com/track/java-clouds">Banshee</a>, 
+    <a href="https://blankbanshee.bandcamp.com/track/b-start-up">Blank</a> 
+    <a href="https://blankbanshee.bandcamp.com/track/java-clouds">Banshee</a>, 
     <a href="https://gost1980s.bandcamp.com/track/amys-wager">GoST</a>, 
     <a href="https://perturbator.bandcamp.com/track/corrupted-by-design">Perturbator</a>, 
     and <a href="https://dreamcatalogue.bandcamp.com/track/the-carving-2">Diamondstein</a>.
 </p>
 <img class="centeredimage" src="images/REAPER/the-end-as-you-perceive-it-to-be-reaper-session.png" />
 <p>
-    It took a long time working through the <a href="studio.php?page=square-peg-releases">Square Peg</a> project to develop a deeper sense for electronic musical instruments, their uses, and their capabilities.  
-    During this time, I spent a lot of time playing with simple free instruments, falling in love with <a href="https://www.blaukraut.info/">BlauKraut's Charlatan</a>, 
+    It took a long time working through the <a href="studio.php?page=square-peg-releases">Square Peg</a> 
+    project to develop a deeper sense for electronic musical instruments, their uses, and their capabilities.  
+    During this time, I spent a lot of time playing with simple free instruments, falling in love with 
+    <a href="https://www.blaukraut.info/">BlauKraut's Charlatan</a>, 
     and learning how to sequence MIDI arrangements inside of REAPER.
 </p>
 <div class="mediaflow">
@@ -29,8 +35,10 @@
     </video>
     <div class="pblock">
         <p>
-            In 2020 I made a move to a longer term living space, and for the first time in 5 years I didn't have to worry about moving out 3 months after I moved in.  
-            I got to work setting up my Bass Station 2 and Minilogue XD from the time for integration with REAPER, and began to produce songs arranged in the box, 
+            In 2020 I made a move to a longer term living space, and for the first time in 5
+             years I didn't have to worry about moving out 3 months after I moved in.  
+            I got to work setting up my Bass Station 2 and Minilogue XD from the time 
+            for integration with REAPER, and began to produce songs arranged in the box, 
             recording takes on the hardware instruments.
         </p>
         <p  style="text-align: left">
@@ -38,9 +46,28 @@
             <audio controls src="audio/techno-february-2022.mp3" />
         </p>
         <p>
-            Three years down this path led me to focusing on hardware instruments, a more hands-on and expressive way to make electronic music.  
+            I began to try and use the hardware sequencers I had available, 
+            although I never got the hang of the sequencer in the bass station.
+            The rhythm dial was not musically intuitive to me, and the step 
+            sequencing was only useful for patterns that always felt too short.  
+            For a while I attempted to use the Bass Station as accompaniment for myself on drum set along with a guitar buddy
+            The sequencer in the Minilogue XD allowed me to discover motion/CC/CV 
+            sequencing and I quickly yearned for a master sequencer that could conduct 
+            an arrangement more sophisticated than the loopy 1-bar patterns.  
+            At the time my MIDI controller was a LaunchKey Mk3, after selling a 
+            Panorama T-series that I couldn't properly fit on my desktop and had an 
+            over 3-month-long support case to try and use the Nektar DAW integration 
+            that ended up in getting ghosted.  I ended up loving the features of 
+            the LaunchKey for standalone instrument control.  The arpeggiator was 
+            unusually inspiring, unlike the similarly designed rhythm knob on 
+            the Bass Station I got rid of.
+        </p>
+        <p>
+            Three years down this path led me to focusing on hardware instruments, a more 
+            hands-on and expressive way to make electronic music.  
             I spent a lot of time jamming out simple sequences on an expanding set of hardware.  
-            Throughout 2023 I spent time learning my instruments and dreaming up more sophisticated ways to make music on that hardware rig.  
+            Throughout 2023 I spent time learning my instruments and dreaming up more 
+            sophisticated ways to make music on that hardware rig.  
             For the first time ever, playing my synth orchestra began to feel like an instrument. 
         </p>
     </div>
@@ -48,10 +75,14 @@
 <p>
     There were two pieces of gear that took my synth desk from a toy to a usable production environment. 
     The Novation SL MkIII, which I'll be talking a lot about, and the Roland TR-8S drum machine.  
-    I'm picky with my drum sounds and while the hardware is great it was unreasonable to synthesize all my drums live, I just couldn't spare the voices.  
-    I had my favorite kick patch on the Minilogue, pinging the filter in resonance with a nice thumpy sweep, but it felt cruel to give up a polysynth for a kick drum. 
-    The TR allows me to sequence my own synthesized drum samples along side the ACB models, FM engines, etc.  I finally could get the drums I love intuitively going while I played.  
-    I used the SL to write almost every song I committed to during the year, and began to appreciate the instant ability to sequence and program new things while playing. 
+    I'm picky with my drum sounds and while the hardware is great it was unreasonable to synthesize 
+    all my drums live, I just couldn't spare the voices.  
+    I had my favorite kick patch on the Minilogue, pinging the filter in resonance with a nice thumpy sweep, 
+    but it felt cruel to give up a polysynth for a kick drum. 
+    The TR allows me to sequence my own synthesized drum samples along side the ACB models, FM engines, etc.  
+    I finally could get the drums I love intuitively going while I played.  
+    I used the SL to write almost every song I committed to during the year, and began to appreciate the 
+    instant ability to sequence and program new things while playing. 
     I'm no keyboard player, and tend to program all melodies and patterns save for fills and mushy ambient-type chords.
 </p>
 <p>
