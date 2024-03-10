c16b87dca60973816fbfeddcde701aef4d37dca3
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-03-10 10:01:15 -0400

    live jam post work

--- webroot/include/post/live-jam-2024-03-09.php
+++ webroot/include/post/live-jam-2024-03-09.php
@@ -12,12 +12,5 @@
 </iframe>
 </div>
 <p>
-    Coming from being a drummer in a groove-oriented metal scene, I have a lot to learn 
-    about what made a good composition when working with pitched instruments.  The work I did under Square Peg has given 
-    me the skills to take my music further down my own path.  I started producing faster and heavier techno styles in 
-    late 2020 and I haven't made much music since then that I feel would fit the Square Peg pathos, but I do plan to 
-    finish that material and release another Square Peg EP sometime not too far down the road.
-</p>
-<p>
-    Check out the <a href="studio?page=square-peg-releases">Releases Highlights</a> page to hear some examples.  I have a goal to add track details for some of the more technically interesting projects.
+    
 </p>
