591f1353d92de91f76b3a0df7a7e32c592ec9419
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-08-07 07:33:50 -0400

    WIP stuff

---
+++ webroot/include/studio/studio-intro.php
@@ -0,0 +1,11 @@
+<h1 class="aqua header">
+    Snailbird Studio
+</h1>
+<p>
+    Here lives a collection of live synth jams done with a party of hardware instruments I've been assembling since 2020.  
+    A few oddball jams to be found, but most are done in my bedroom studio, recorded in multichannel through the old mixer I used to record my metal band's demos in highschool.
+</p>
+<p>
+    A lot of the music I've been making is inspired by the spirit of long-form club sets, an environment where time no longer exists and each down beat is its own universe, connected to all others.
+    I've found a deep sense of safety inside the thudding walls of dark basements and old structures because of the way this music turns
+</p>
